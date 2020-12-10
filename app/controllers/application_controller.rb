3712f2353d292eb5caaf5d8d4b5e822ca0f0d74c
SHirosukesan <darumesian43@gmail.com>
2020-12-10 20:50:52 +0900

    Choice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+#基本的に外部からは隠蔽するが、サブクラスやパッケージ内からのみ利用させたい場合は protected 。 基本的に外部からは隠蔽し、クラス内からのみ利用させたい場合は private 。 という使い分けになっていて、かなり分かりやすく、使い分ける目的も明確です。
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+#sign upに名前の項目を追加下から記入した。
 end
