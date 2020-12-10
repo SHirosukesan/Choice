840a523ca2311c003d49c80edd8359b70ae53472
SHirosukesan <darumesian43@gmail.com>
2020-12-11 03:28:50 +0900

    deveice-controller-add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 before_action :configure_permitted_parameters, if: :devise_controller?
+#devise利用の機能（ユーザ登録、ログイン認証など）が使われる場合、その前にconfigure_permitted_parametersが実行されます。
 
   protected
 #基本的に外部からは隠蔽するが、サブクラスやパッケージ内からのみ利用させたい場合は protected 。 基本的に外部からは隠蔽し、クラス内からのみ利用させたい場合は private 。 という使い分けになっていて、かなり分かりやすく、使い分ける目的も明確です。
@@ -7,5 +8,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+#8~10行目でnameのデータ操作を許可するアクションメソッドが指定
 #sign upに名前の項目を追加下から記入した。
 end
---
+++ app/controllers/shops_controller.rb
@@ -0,0 +1,13 @@
+class ShopsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
