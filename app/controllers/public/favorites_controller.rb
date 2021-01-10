0106c824b158c029514b779708745301fdebb61c
SHirosukesan <darumesian43@gmail.com>
2021-01-10 17:09:13 +0900

    非同期修正中

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,14 @@
 class Public::FavoritesController < ApplicationController
   def create
-  	@shop = shop = Shop.find(params[:id])
     # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
     # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
-    favorite = current_user.favorites.build(shop_id: params[:shop_id])
-    favorite.save
+    @favorite = current_user.favorites.build(shop_id: params[:shop_id])
+    @favorite.save
   end
 
   def destroy
-    @shop = Shop.find(params[:id])
-  	favorite = Favorite.find_by(shop_id: params[:shop_id], user_id: current_user.id)
-    favorite.destroy
+  	@favorite = Favorite.find_by(shop_id: params[:shop_id], user_id: current_user.id)
+    @favorite.destroy
   end
 end
